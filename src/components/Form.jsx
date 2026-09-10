8bcf8d5e75a7aa3b55995e6153562b53e4cb8c3a
Blade21Shade <blade21shade@gmail.com>
2026-09-10 12:09:13 -0500

    Create initial layout for Form
    Form.jsx
    - Personal information is handled here since an array isn't needed to store the information
    - WorkHistory and Education will both use arrays to store entries, so they use components

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,5 +2,44 @@ import EducationFormEntry from "./EducationFormEntry";
 import WorkHistoryFormEntry from "./WorkHistoryFormEntry";
 
 export default function Form({data, setLoadFormIfTrue}) {
-    
+    return (
+    <form>
+        <div>
+            <h1>Personal Information</h1>
+            <div id="nameContainer">
+                <label htmlFor="firstName">First Name: </label>
+                <input type="text" name="firstName" id='firstName'></input>
+                <label htmlFor="lastName">Last Name: </label>
+                <input type="text" name="lastName" id="lastName"></input>
+            </div>
+            <div id="concatInfoContainer">
+                <label htmlFor="email">Email: </label>
+                <input type="email" name="email" id="email"></input>
+                <label htmlFor="phone">Phone: </label>
+                <input type="tel" name="phone" id="phone"></input>
+            </div>
+        </div>
+        <div>
+            <h1>Work History</h1>
+            {data.workHistory.map((entry, index) => (
+                <WorkHistoryFormEntry
+                    key={index}
+                    workHistoryObject={entry}>
+                </WorkHistoryFormEntry>
+            ))}
+            <button>Add Work History</button>
+        </div>
+        <div>
+            <h1>Education</h1>
+            {data.education.map((entry, index) => (
+                <EducationFormEntry
+                    key={index}
+                    educationObject={entry}>
+                </EducationFormEntry>
+            ))}
+            <button>Add Education</button>
+        </div>
+        <button type="submit">Submit</button>
+    </form>
+    )
 }
