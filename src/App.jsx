e3b32769b2e1a0f956ab12bd4e5b6e5361d3f5ee
Blade21Shade <blade21shade@gmail.com>
2026-09-14 11:06:45 -0500

    Update Form component to work with data
    The component is now designed to work with a data prop passed to it.
    - Sets initial personal info and contact info from data fields
    - Uses child components to create work history and education
     - These components are currently still skeletal, so they don't function here
    - Some helper functions are used to update the data object passed in
    
    App.jsx
    - Initializes the 'data' state and passes it to the Form component
    
    Form.jsx
    - Uses and updates the 'data' state from App
    - Created addEntry(workOrEducation)
     - Adds a new work history or education entry to the data state
    - Created deleteEntry(workOrEducation, id)
     - Delete the work history or education entry from the data state matching the given ID
    - Created updatePersonalInformation(newVal, whatsUpdating)
     - Updates the personal information fields in data state
      - Name, email, or phone
    - Updated the returned <form>
     - <input>s work with data state and are required
     - Work history and education divs
      - The key change (from index to an id field) was needed because the storage method was changed but I don't believe it was documented
      - Delete entry is passed as the form entry will have a button to delete 'this' entry
       - Again, the form entries are just skeletons at the moment
      - The buttons for adding new entries now actually do so

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import Form from './components/Form'
 import Application from './components/Application'
+import { getDataTemplate } from './dataTemplates'
 
 import './App.css'
 
 function App() {
   const [loadFormIfTrue, setLoadFormIfTrue] = useState(true);
+  const [data, setData] = useState(getDataTemplate());
 
   if (loadFormIfTrue) {
-    return <Form setLoadFormIfTrue={setLoadFormIfTrue}></Form>
+    return <Form setLoadFormIfTrue={setLoadFormIfTrue} data={data} updateData={setData}></Form>
   } else {
     return <Application setLoadFormIfTrue={setLoadFormIfTrue}></Application>
   }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,45 +1,96 @@
 import EducationFormEntry from "./EducationFormEntry";
 import WorkHistoryFormEntry from "./WorkHistoryFormEntry";
+import { getEducationTemplate, getWorkHistoryTemplate } from "../dataTemplates";
 
-export default function Form({data, setLoadFormIfTrue}) {
+export default function Form({data, setLoadFormIfTrue, updateData}) {
+    function addEntry(workOrEducation) {
+        let updatedData = {...data};
+        let id = crypto.randomUUID();
+        
+        if (workOrEducation === 'work') {
+            updatedData.workHistory.push({id: id, workHistory: getWorkHistoryTemplate()});
+        } else if (workOrEducation === 'education') {
+            updateData.education.push({id: id, education: getEducationTemplate()});
+        }
+
+        updateData(updatedData);
+    }
+    
+    function deleteEntry(workOrEducation, id) {
+        let updatedData = {...data};
+        let arrayToSearch;
+
+        if (workOrEducation === 'work') {
+            arrayToSearch = updatedData.workHistory;
+        } else if (workOrEducation === 'education') {
+            arrayToSearch = updatedData.education;
+        } else {
+            return;
+        }
+        
+        arrayToSearch = arrayToSearch.filter((entry) => {
+            entry.id != id;
+        });
+
+        updateData(updatedData);
+    }
+
+    function updatePersonalInformation(newVal, whatsUpdating) {
+        let updatedData = {...data};
+
+        if (whatsUpdating === 'firstName') {
+            updatedData.firstName = newVal;
+        } else if (whatsUpdating === 'lastName') {
+            updatedData.lastName = newVal;
+        } else if (whatsUpdating === 'email') {
+            updatedData.email = newVal;
+        } else if (whatsUpdating === 'phone') {
+             updatedData.phone = newVal;
+        }
+
+        updateData(updatedData);
+    }
+    
     return (
     <form>
         <div>
             <h1>Personal Information</h1>
             <div id="nameContainer">
                 <label htmlFor="firstName">First Name: </label>
-                <input type="text" name="firstName" id='firstName'></input>
+                <input type="text" name="firstName" id='firstName' onChange={(e)=> updatePersonalInformation(e.target.value, 'firstName')} required>{data.firstName}</input>
                 <label htmlFor="lastName">Last Name: </label>
-                <input type="text" name="lastName" id="lastName"></input>
+                <input type="text" name="lastName" id="lastName" onChange={(e)=> updatePersonalInformation(e.target.value, 'lastName')} required>{data.lastName}</input>
             </div>
-            <div id="concatInfoContainer">
+            <div id="contactInfoContainer">
                 <label htmlFor="email">Email: </label>
-                <input type="email" name="email" id="email"></input>
+                <input type="email" name="email" id="email" autoComplete="email" onChange={(e)=> updatePersonalInformation(e.target.value, 'email')} required>{data.email}</input>
                 <label htmlFor="phone">Phone: </label>
-                <input type="tel" name="phone" id="phone"></input>
+                <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="phone" id="phone" autoComplete="off" onChange={(e)=> updatePersonalInformation(e.target.value, 'phone')} required>{data.phone}</input>
             </div>
         </div>
         <div>
             <h1>Work History</h1>
-            {data.workHistory.map((entry, index) => (
+            {data.workHistory.map((entry) => (
                 <WorkHistoryFormEntry
-                    key={index}
-                    workHistoryObject={entry}>
+                    key={entry.id}
+                    workHistoryObject={entry}
+                    deleteEntry={deleteEntry}>
                 </WorkHistoryFormEntry>
             ))}
-            <button>Add Work History</button>
+            <button onClick={() => addEntry('createNewWorkHistoryEntry')}>Add Work History</button>
         </div>
         <div>
             <h1>Education</h1>
-            {data.education.map((entry, index) => (
+            {data.education.map((entry) => (
                 <EducationFormEntry
-                    key={index}
-                    educationObject={entry}>
+                    key={entry.id}
+                    educationObject={entry}
+                    deleteEntry={deleteEntry}>
                 </EducationFormEntry>
             ))}
-            <button>Add Education</button>
+            <button onClick={() => addEntry('createNewEducationEntry')}>Add Education</button>
         </div>
-        <button type="submit">Submit</button>
+        <button onClick={() => setLoadFormIfTrue(false)}>Submit</button>
     </form>
     )
 }
