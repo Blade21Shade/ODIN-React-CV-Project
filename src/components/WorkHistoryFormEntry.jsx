65f52a133bb389e7fbf157aaaf196d8145778306
Blade21Shade <blade21shade@gmail.com>
2026-09-15 18:05:53 -0500

    Create code for WorkHistoryFormEntry.jsx
    This is used in Form for users to create their work history
    
    WorkHistoryFormEntry.jsx
    - Update props to match values passed in Form.jsx
    - Returned value
     -  Inputs for company name, position title, start date, and end date
      - When creating one of these entries the start and end date fields are empty, which initializes the date picker in the browser to a 'default format'; this isn't an issue, but it may be better to have a placeholder value instead
       - I didn't want to make a new Date object for every instance of this component on every render since that seemed inefficient (even though that probably doesn't matter here) but couldn't decide on a suitable placeholder, so for now I'm leaving it as it
     - Button for deleting this entry
    - Create function updateWorkHistory(newVal, whatsUpdating)
     - Updates the field associated with whatsUpdating to newVal

--- src/components/WorkHistoryFormEntry.jsx
+++ src/components/WorkHistoryFormEntry.jsx
@@ -1,3 +1,63 @@
-export default function WorkHistoryFormEntry({key, workHistoryObject}) {
+import { deepCopyData } from "../dataTemplates";
 
+export default function WorkHistoryFormEntry({data, updateData, workHistoryObject, deleteEntry}) {
+
+    let workHistory = workHistoryObject.workHistory;
+    let id = workHistoryObject.id;
+
+    function updateWorkHistory(newVal, whatsUpdating) {
+        let updatedData = deepCopyData(data);
+        
+        let newWorkHistory = {...workHistory};
+
+        if (whatsUpdating === 'company') {
+            newWorkHistory.company = newVal;
+        } else if (whatsUpdating === 'position') {
+            newWorkHistory.position = newVal;
+        } else if (whatsUpdating === 'startDate') {
+            newWorkHistory.startDate = newVal;
+        } else if (whatsUpdating === 'endDate') {
+            newWorkHistory.endDate = newVal;
+        } else {
+            return;
+        }
+
+        updatedData.workHistory.forEach(element => {
+            if (id === element.id) {
+                element.workHistory = newWorkHistory;
+            }
+        });
+
+        updateData(updatedData);
+    }
+
+    return(
+        <div>
+            <label htmlFor="company">Company: </label>
+            <input name="company" id="company" type="text" value={workHistory.company} onChange={(e)=>updateWorkHistory(e.target.value, 'company')}></input>
+
+            <label htmlFor="position">Position: </label>
+            <input name="position" id="position" type="text" value={workHistory.position} onChange={(e)=>updateWorkHistory(e.target.value, 'position')}></input>
+
+            <label htmlFor="startDate">Start Date: </label>
+            <input
+                name="startDate"
+                id="startDate"
+                type="date"
+                value={workHistory.startDate}
+                onChange={(e)=>updateWorkHistory(e.target.value, 'startDate')}>
+            </input>
+
+            <label htmlFor="endDate">End Date: </label>
+            <input
+                name="endDate"
+                id="endDate"
+                type="date"
+                value={workHistory.endDate}
+                onChange={(e)=>updateWorkHistory(e.target.value, 'endDate')}>
+            </input>
+
+            <button type='button' onClick={() => deleteEntry('work', id)}>Delete</button>
+        </div>
+    )
 }
